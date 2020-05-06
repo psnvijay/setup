b4a9d2dd41a3122a1010389fc7215767ebc552f9
Vijay Pappu <vijayp@spotify.com>
2020-05-06 11:14:20 -0400

    add teardown file

---
+++ teardown.sh
@@ -0,0 +1,5 @@
+#################################
+# 0. Package manager - Homebrew
+#################################
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+
